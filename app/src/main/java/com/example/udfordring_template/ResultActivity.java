7618afb3b700444676884b7b2450650711be4f8d
Aschelde <asger.foulum@gmail.com>
2021-06-16 11:32:28 +0200

    Added button listeners in ResultActivity

--- app/src/main/java/com/example/udfordring_template/ResultActivity.java
+++ app/src/main/java/com/example/udfordring_template/ResultActivity.java
@@ -2,8 +2,11 @@ package com.example.udfordring_template;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -18,5 +21,27 @@ public class ResultActivity extends AppCompatActivity {
         resultText.setText(result);
     }
 
+    public void addListenerOnButton(){
+        Button retryButton = (Button) findViewById(R.id.retry);
+        Button returnButton = (Button) findViewById(R.id.return_button);
 
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Replace MainActivity with the MapActivity/Main activity in DTU-GO
+                //https://stackoverflow.com/questions/14001963/finish-all-activities-at-a-time
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+            }
+        });
+    }
 }
