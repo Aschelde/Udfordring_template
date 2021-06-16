93c23f771b5c02443b8c4375d3944903c657d9d2
Aschelde <asger.foulum@gmail.com>
2021-06-16 09:57:24 +0200

    commit

--- app/src/main/java/com/example/udfordring_template/MainActivity.java
+++ app/src/main/java/com/example/udfordring_template/MainActivity.java
@@ -1,39 +1,71 @@
 package com.example.udfordring_template;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
-
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private boolean counterIsRunning = false;
+    private SensorManager sensorManager;
+    private TextView textViewStepCounter, textViewStepDetector;
+    private Sensor mStepCounter;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
 
         @Override
         protected void onCreate (Bundle savedInstanceState){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+                //ask for permission
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+            }
             super.onCreate(savedInstanceState);
             getSupportActionBar().hide(); //hide the title bar
             setContentView(R.layout.activity_main);
             addListenerOnButton();
+
+            textViewStepCounter = findViewById(R.id.Parameter);
+
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+            if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+
+                mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+                isCounterSensorPresent = true;
+
+            } else {
+                textViewStepCounter.setText("Counter is not present");
+                isCounterSensorPresent = false;
+            }
+
         }
 
     public void addListenerOnButton(){
-        Button counterButton = (Button) findViewById(R.id.Counter1);
+        Button counterButton = (Button) findViewById(R.id.Counter);
+        counterButton.setEnabled(true);
+        counterButton.setText("START");
         TextView textView = (TextView) findViewById(R.id.Parameter);
         counterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 counterButton.setEnabled(false);
+                counterIsRunning = true;
+                onResume();
 
-                new CountDownTimer(30000, 1000) {
+                new CountDownTimer(5000, 1000) {
 
                     public void onTick(long millisUntilFinished) {
                         counterButton.setText("" + millisUntilFinished / 1000);
@@ -41,13 +73,61 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     public void onFinish() {
-                        //counterButton.setText("done!");
                         Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                        int result = 333;
+                        intent.putExtra("RESULT", result);
+                        counterIsRunning = false;
+                        onPause();
                         startActivity(intent);
                     }
 
                 }.start();
+
+
             }
         });
+    }
+
+    private void startChallenge() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+            sensorManager.registerListener(this,mStepCounter, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        if (!counterIsRunning) {
+            addListenerOnButton();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER ) != null){
+            sensorManager.unregisterListener(this,mStepCounter);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.sensor == mStepCounter){
+            stepCount = (int) event.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
--- app/src/main/java/com/example/udfordring_template/ResultActivity.java
+++ app/src/main/java/com/example/udfordring_template/ResultActivity.java
@@ -2,13 +2,52 @@ package com.example.udfordring_template;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+    TextView resultText = (TextView) findViewById(R.id.Result_text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_result);
+
+        //Intent mIntent = getIntent();
+        //int result = mIntent.getIntExtra("RESULT", 0);
+        //resultText.setText("" + result);
+
+        addListenerOnButton();
+    }
+
+
+
+    public void addListenerOnButton(){
+        Button retryButton = (Button) findViewById(R.id.retry_button);
+        Button returnButton = (Button) findViewById(R.id.return_button);
+
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Replace MainActivity with the MapActivity/Main activity in DTU-GO
+                //https://stackoverflow.com/questions/14001963/finish-all-activities-at-a-time
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+            }
+        });
     }
 }
